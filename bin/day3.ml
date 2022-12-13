a10901f1908282bcd07d401831c3c982feaba27a
dansusman <frenchminister@gmail.com>
2022-12-13 15:27:51 -0500

    Day 3 Part 2

--- bin/day3.ml
+++ bin/day3.ml
@@ -21,3 +21,19 @@ let part1 acc s =
 let () = print_endline "Part 1"
 let () = List.fold_left part1 0 result |> Printf.printf "%d \n"
 
+let part2 elves =
+  let get_score x y z =
+    let x = set_of_char x in
+    let y = set_of_char y in
+    let z = set_of_char z in
+    let intersect = CharSet.inter x y |> CharSet.inter z |> CharSet.choose in
+    character_score intersect in
+  let rec fold3 func l acc =
+    match l with
+    | [] -> acc
+    | car :: cadr :: caddr :: cdr -> fold3 func cdr (acc + (func car cadr caddr))
+    | _ -> assert false in
+  fold3 get_score elves 0
+
+let () = print_endline "Part 2"
+let () = part2 result |> Printf.printf "%d \n"
