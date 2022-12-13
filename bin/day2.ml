6850e1e6ca5cbbe25552534926161a6d9f9d6814
dansusman <frenchminister@gmail.com>
2022-12-13 13:35:58 -0500

    Day 2 Part 2

--- bin/day2.ml
+++ bin/day2.ml
@@ -1,6 +1,14 @@
 let result = Aoc22.read_lines "day2.txt"
 
-let get_score s =
+let score input func =
+  let rec helper input func acc =
+    match input with
+    | [] -> acc
+    | car :: cdr -> helper cdr func ((func car) + acc) in
+  helper input func 0
+
+(* Part 1 *)
+let get_score1 s =
   let elf =
   match String.sub s 0 1 with
   | "A" -> "X"
@@ -21,11 +29,52 @@ let get_score s =
     | _ (* loss *) -> 0 in
   shape_score + outcome_score
 
-let score input =
-  let rec helper input acc =
-    match input with
-    | [] -> acc
-    | car :: cdr -> helper cdr (get_score car + acc) in
-  helper input 0
+let () = print_endline "Part 1"
+let () = score result get_score1 |> string_of_int |> print_endline
 
-let () = score result |> string_of_int |> print_endline
+(* Part 2 *)
+module RPS = struct
+  type t =
+    | Rock
+    | Paper
+    | Scissors
+
+  let to_string = function
+    | Rock -> "A"
+    | Paper -> "B"
+    | Scissors -> "C"
+  
+  let from_string = function
+    | "A" -> Rock
+    | "B" -> Paper
+    | "C" -> Scissors
+    | _ -> assert false
+
+  let compare a b = compare (to_string a) (to_string b)
+end
+
+module RPSMap = Map.Make (RPS)
+module StringMap = Map.Make (String)
+
+let rps_map s = List.to_seq s |> RPSMap.of_seq
+let str_map l = List.to_seq l |> StringMap.of_seq
+
+(* They play x, I win/lose by playing y*)
+let win = rps_map [RPS.Rock, RPS.Paper; RPS.Paper, RPS.Scissors; RPS.Scissors, RPS.Rock]
+let lose = rps_map [RPS.Rock, RPS.Scissors; RPS.Paper, RPS.Rock; RPS.Scissors, RPS.Paper]
+let outcome_score = str_map ["X", 0; "Y", 3; "Z", 6]
+let shape_score = rps_map [RPS.Rock, 1; RPS.Paper, 2; RPS.Scissors, 3]
+
+let get_score2 s =
+  let elf = String.sub s 0 1 |> RPS.from_string in
+  let goal = String.sub s 2 1 in
+  let result =
+    match goal with
+    | "X" (* I need to lose *) -> RPSMap.find elf lose
+    | "Y" (* I need to draw *) -> elf
+    | _ (* I need to win *) -> RPSMap.find elf win in
+  StringMap.find goal outcome_score + RPSMap.find result shape_score
+  
+
+let () = print_endline "Part 2"
+let () = score result get_score2 |> string_of_int |> print_endline
